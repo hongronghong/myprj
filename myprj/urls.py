f3651eec5d7252aa61deb890c52b9cce95fa49cc
hongronghong <315152752@qq.com>
2015-10-08 23:03:40 +0800

    增加一个hello django页面

--- myprj/urls.py
+++ myprj/urls.py
@@ -18,4 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-]
+    url(r'^$', include('app1.urls', namespace="app1")),
+
+    url(r'^app1/',include('app1.urls', namespace="app1")),
+    ]
